7ba7da82be95fdb97242d963b224aae3aa5e8bfc
Shubham Pandey <shubhampcpandey13@gmail.com>
2022-07-27 14:24:57 +0530

    update metric macro syntax as per dbt-metrics pre release

--- models/metrics/hubspot__weekly_email_open_events.sql
+++ models/metrics/hubspot__weekly_email_open_events.sql
@@ -1,8 +1,8 @@
 -- depends_on: {{ var('email_event') }}
 
 select *
-from {{ metrics.metric(
-    metric_name='hubspot__weekly_email_open_events',
+from {{ metrics.calculate(
+    metric('hubspot__weekly_email_open_events'),
     grain='week',
     dimensions=[],
     secondary_calculations=[]
