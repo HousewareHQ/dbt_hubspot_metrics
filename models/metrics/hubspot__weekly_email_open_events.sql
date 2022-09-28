71ccda585f6b40ccd374b3b70afdf0fabfd87555
Aryan Verma <54755677+AryanMadhavVerma@users.noreply.github.com>
2022-09-28 11:59:19 +0530

    Update/metrics 0.3.5 (#5)
    * update to dbt_metrics_0.3.2
    
    * update/metrics_0.3.5
    
    Co-authored-by: Aryan Verma <aryanverma@Aryans-MacBook-Air.local>

--- models/metrics/hubspot__weekly_email_open_events.sql
+++ models/metrics/hubspot__weekly_email_open_events.sql
@@ -1,8 +1,8 @@
 -- depends_on: {{ ref('hubspot__email_events') }}
 
 select *
-from {{ metrics.metric(
-    metric_name='hubspot__weekly_email_open_events',
+from {{ metrics.calculate(
+    metric('hubspot__weekly_email_open_events'),
     grain='week',
     dimensions=[],
     secondary_calculations=[]
